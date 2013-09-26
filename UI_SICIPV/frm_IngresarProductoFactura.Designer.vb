c75f7e54fa9157c1ec86cb1936f552c8b27e98ae
jent46 <jorgefren12@gmail.com>
2013-09-26 16:30:50 -0500

    ngresar factura

---
+++ UI_SICIPV/frm_IngresarProductoFactura.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_IngresarProductoFactura
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.gbBuscar = New System.Windows.Forms.GroupBox()
+        Me.dgvBusqueda = New System.Windows.Forms.DataGridView()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.txtBusqueda = New System.Windows.Forms.TextBox()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.gbBuscar.SuspendLayout()
+        CType(Me.dgvBusqueda, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'gbBuscar
+        '
+        Me.gbBuscar.Controls.Add(Me.dgvBusqueda)
+        Me.gbBuscar.Controls.Add(Me.Label5)
+        Me.gbBuscar.Controls.Add(Me.btnBuscar)
+        Me.gbBuscar.Controls.Add(Me.txtBusqueda)
+        Me.gbBuscar.Location = New System.Drawing.Point(5, 2)
+        Me.gbBuscar.Name = "gbBuscar"
+        Me.gbBuscar.Size = New System.Drawing.Size(464, 315)
+        Me.gbBuscar.TabIndex = 25
+        Me.gbBuscar.TabStop = False
+        Me.gbBuscar.Text = "Busqueda Productos"
+        '
+        'dgvBusqueda
+        '
+        Me.dgvBusqueda.AllowUserToAddRows = False
+        Me.dgvBusqueda.AllowUserToDeleteRows = False
+        Me.dgvBusqueda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvBusqueda.Location = New System.Drawing.Point(15, 79)
+        Me.dgvBusqueda.Name = "dgvBusqueda"
+        Me.dgvBusqueda.ReadOnly = True
+        Me.dgvBusqueda.Size = New System.Drawing.Size(432, 208)
+        Me.dgvBusqueda.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(15, 38)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 13)
+        Me.Label5.TabIndex = 22
+        Me.Label5.Text = "Producto:"
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(405, 28)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(26, 23)
+        Me.btnBuscar.TabIndex = 4
+        Me.btnBuscar.Text = "..."
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'txtBusqueda
+        '
+        Me.txtBusqueda.Location = New System.Drawing.Point(74, 31)
+        Me.txtBusqueda.Name = "txtBusqueda"
+        Me.txtBusqueda.Size = New System.Drawing.Size(313, 20)
+        Me.txtBusqueda.TabIndex = 3
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'frm_IngresarProductoFactura
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 328)
+        Me.Controls.Add(Me.gbBuscar)
+        Me.Name = "frm_IngresarProductoFactura"
+        Me.Text = "frm_IngresarProductoFactura"
+        Me.gbBuscar.ResumeLayout(False)
+        Me.gbBuscar.PerformLayout()
+        CType(Me.dgvBusqueda, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents gbBuscar As System.Windows.Forms.GroupBox
+    Friend WithEvents dgvBusqueda As System.Windows.Forms.DataGridView
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents btnBuscar As System.Windows.Forms.Button
+    Friend WithEvents txtBusqueda As System.Windows.Forms.TextBox
+    Friend WithEvents ErrorProvider1 As System.Windows.Forms.ErrorProvider
+End Class
